839bb4044294112deb9ee529f6624a16d9fc863a
JyJyJcr <82190170+JyJyJcr@users.noreply.github.com>
2024-03-29 16:29:10 +0900

    refactor: add srcdoc

--- src/capacity.rs
+++ src/capacity.rs
@@ -1,2 +1,4 @@
+/// 実数型
 pub type FL = f64;
+/// 非負整数型
 pub type SI = usize;
--- src/qcd.rs
+++ src/qcd.rs
@@ -1,11 +1,15 @@
 use crate::{capacity::*, monte::*};
 use ndarray::*;
+/// QCDの状態(3空間+1虚時間の4次元)
 struct QCDState<const X: SI, const Y: SI, const Z: SI, const T: SI> {
+    /// クォーク場
     f: Array4<FL>,
+    /// グルーオン場
     u: Array4<FL>,
 }
 impl<const X: SI, const Y: SI, const Z: SI, const T: SI> QCDState<X, Y, Z, T> {
-    fn new() -> Self {
+    /// 真空状態を生成
+    fn zeros() -> Self {
         QCDState::<X, Y, Z, T> {
             f: ndarray::Array4::zeros((X, Y, Z, T)),
             u: ndarray::Array4::zeros((X, Y, Z, T)),
@@ -13,10 +17,13 @@ impl<const X: SI, const Y: SI, const Z: SI, const T: SI> QCDState<X, Y, Z, T> {
     }
 }
 
+/// `QCDState`用の`StateTheos`
 struct QCDTheos<const X: SI, const Y: SI, const Z: SI, const T: SI> {
+    /// 擬時間発展の時間幅
     virt_dt: FL,
 }
 impl<const X: SI, const Y: SI, const Z: SI, const T: SI> QCDTheos<X, Y, Z, T> {
+    /// 擬時間発展の時間幅を`virt_dt`に設定して生成
     fn new(virt_dt: FL) -> Self {
         QCDTheos::<X, Y, Z, T> { virt_dt: virt_dt }
     }
@@ -24,10 +31,12 @@ impl<const X: SI, const Y: SI, const Z: SI, const T: SI> QCDTheos<X, Y, Z, T> {
 impl<const X: SI, const Y: SI, const Z: SI, const T: SI> StateTheos<QCDState<X, Y, Z, T>>
     for QCDTheos<X, Y, Z, T>
 {
+    /// `QCDState`の生成、今は真空を返す
     fn generate(&self) -> Result<QCDState<X, Y, Z, T>, TheosFail> {
-        Ok(QCDState::new())
+        Ok(QCDState::zeros())
     }
 
+    /// `QCDState`の擬時間発展、今は何もしない
     fn next(&self, s: &mut QCDState<X, Y, Z, T>) -> Result<(), TheosFail> {
         //currently do nothing
         Ok(())
@@ -36,25 +45,28 @@ impl<const X: SI, const Y: SI, const Z: SI, const T: SI> StateTheos<QCDState<X, 
 }
 
 use clap::Parser;
+/// `qcd`モジュールのオプション
 #[derive(Parser, Debug)]
 pub struct Opt {
-    /// steps
+    /// 擬時間発展のステップ数
     steps: SI,
 }
 
+/// `qcd`モジュールのコンフィグ
 struct Conf {
     x: SI,
     y: SI,
     z: SI,
     t: SI,
 }
+/// `qcd`モジュールのコンフィグ定数(コンパイル時設定)
 const CONF: Conf = Conf {
     x: 10,
     y: 10,
     z: 10,
     t: 10,
 };
-
+/// `qcd`モジュールのシミュレーション実行
 pub fn run(opt: Opt) -> Result<(), TheosFail> {
     let th = QCDTheos::<{ CONF.x }, { CONF.y }, { CONF.z }, { CONF.t }>::new(1.0e-8);
     let mut s = th.generate()?;
